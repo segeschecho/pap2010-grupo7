366b72504c20362470f9d69e110abacfdf974902
stleye <stleye@e3625ad9-2858-1949-73a1-c9317d294030>
2010-04-04 02:20:34 +0000

    Increiblemente mejorado pero no suficiente. Sigue sin pasar los tests

--- tp1/codigo/ejercicio2/main.cpp
+++ tp1/codigo/ejercicio2/main.cpp
@@ -7,85 +7,9 @@
 //
 using namespace std;
 
-typedef vector<vector<int> > tTablero;
+typedef long entero;
 
-struct tAlfil{
-    bool enTablero;
-    int fila;
-    int columna;
-};
-
-const bool desocupado = 0;
-const bool ocupado = 1;
-
-bool esAtacado(vector<tAlfil>& alfiles, int i, int j){
-
-	int cantAlfiles = alfiles.size();
-    int indice;
-
-    int k;
-    int l;
-
-    for (indice=0; indice<cantAlfiles; indice++){
-
-        if (alfiles[indice].enTablero){
-
-            k = alfiles[indice].fila;
-            l = alfiles[indice].columna;
-
-            if (i-j == k-l || i+j == k+l || j-l == i-k || j-l == k-i){
-                return true;
-            }
-        }
-    }
-
-	return false;
-}
-
-//devuelve true si la primera posicion es menor a la segunda posicion en una matriz
-bool menor(int fila1, int columna1, int fila2, int columna2){
-
-    if (fila1 < fila2){
-        return true;
-    }
-    else{
-        if (fila1 > fila2){
-            return false;
-        }
-        else{
-            if (columna1 < columna2)
-                return true;
-            else
-                return false;
-        }
-    }
-}
-
-bool seRespetanPosicionesRelativas(vector<tAlfil>& alfiles, int i, int j, int alfil){
-
-	int cantAlfiles = alfiles.size();
-    int k;
-
-    for (k=0; k<alfil; k++){
-        //PosicionalfilIndice > posicionAlfil
-        if (alfiles[k].enTablero && !(menor(alfiles[k].fila, alfiles[k].columna, i, j))){
-            return false;
-        }
-    }
-
-    for (k=alfil+1; k<cantAlfiles; k++){
-        //PosicionalfilIndice < posicionAlfil
-        if (alfiles[k].enTablero && menor(alfiles[k].fila, alfiles[k].columna, i, j)){
-            return false;
-        }
-    }
-
-    return true;
-}
-
-
-void go(tTablero& t, vector<tAlfil>& alfiles, int &k, int &n, long& cantSoluciones, int cantAlfilesColocados, int ultimoAlfilPuesto, int ultimaFila){
-//void go(tTablero& t, vector<tAlfil>& alfiles, int &k, int &n, long& cantSoluciones, int cantAlfilesColocados, int ultimoAlfilPuesto){
+void go(vector<bool>& DiagonalSubida, vector<bool>& DiagonalBajada, int &k, int &n, entero& cantSoluciones, int cantAlfilesColocados, int ultimaFila, int ultimaColumna){
 
 	int i,j;
 
@@ -93,69 +17,52 @@ void go(tTablero& t, vector<tAlfil>& alfiles, int &k, int &n, long& cantSolucion
 		cantSoluciones++;
 	}else{
 
-		//for (i=ultimaFila; i<n; i++){
-		for (i=0; i<n; i++){
-			for (j=0; j<n; j++){
+		j = ultimaColumna;
+		for (i=ultimaFila; i<n; i++){
+			while (j<n){
 
-				if (t[i][j] == desocupado && (!esAtacado(alfiles,i,j)) && seRespetanPosicionesRelativas(alfiles,i,j,ultimoAlfilPuesto)){
+				if (!DiagonalSubida[i+j] && !DiagonalBajada[i-j+n-1]){
 
-					t[i][j] = ultimoAlfilPuesto;
-					alfiles[ultimoAlfilPuesto].enTablero = true;
-					alfiles[ultimoAlfilPuesto].fila = i;
-					alfiles[ultimoAlfilPuesto].columna = j;
+					DiagonalSubida[i+j] = true;
+					DiagonalBajada[i-j+n-1] = true;
 
-					go(t,alfiles,k,n,cantSoluciones,cantAlfilesColocados+1,ultimoAlfilPuesto+1, i);
-					//go(t,alfiles,k,n,cantSoluciones,cantAlfilesColocados+1,ultimoAlfilPuesto+1);
+					go(DiagonalSubida, DiagonalBajada,k,n,cantSoluciones,cantAlfilesColocados+1, i, j+1);
 
-					t[i][j] = desocupado;
-					alfiles[ultimoAlfilPuesto].enTablero = false;
-					alfiles[ultimoAlfilPuesto].fila = -1;
-					alfiles[ultimoAlfilPuesto].columna = -1;
+					DiagonalSubida[i+j] = false;
+					DiagonalBajada[i-j+n-1] = false;
 
 				}
+				j++;
 			}
+			j = 0;
 		}
 	}
 }
 
-long cantSoluciones(int n, int k){
+entero cantSoluciones(int n, int k){
 
 	if (k > n*n){
 		return 0;
 	}else{
 
-	    tTablero tablero;
-		int i, j;
 
-		//Cargo la matriz con todos false
-		tablero.resize(n);
+		vector<bool> DiagonalSubida;
+		vector<bool> DiagonalBajada;
+
+		DiagonalBajada.resize((2*n)-1);
+		DiagonalSubida.resize((2*n)-1);
+
+		int i;
 
 		for (i = 0; i<n; i++){
-			tablero[i].resize(n);
+			DiagonalSubida[i] = false;
+			DiagonalBajada[i] = false;
 		}
 
-		for (i = 0; i<n; i++){
-			for (j = 0; j<n; j++){
-				tablero[i][j] = desocupado;
-			}
-		}
-
-		vector<tAlfil> alfiles;
-
-		//Cargo la matriz con todos false
-		alfiles.resize(n);
-
-		for (i=0; i<n; i++){
-		    alfiles[i].fila = -1;
-		    alfiles[i].columna = -1;
-		    alfiles[i].enTablero = false;
-		}
-
-		long cantSoluciones = 0;
+		entero cantSoluciones = 0;
 		int cantAlfilesColocados = 0;
-		int ultimoAlfilPuesto = 0;
 
-		go(tablero,alfiles,k,n,cantSoluciones,cantAlfilesColocados, ultimoAlfilPuesto, 0);
+		go(DiagonalSubida,DiagonalBajada,k,n,cantSoluciones,cantAlfilesColocados, 0, 0);
 
 		return cantSoluciones;
 	}
@@ -163,16 +70,18 @@ long cantSoluciones(int n, int k){
 
 int main(int argc, char* argv[])
 {
-    int n, k;
+    /*int n, k;
     scanf("%i %i", &n, &k);
 
-    long cant;
+    entero cant;
 
     while (n != 0 || k != 0){
         cant = cantSoluciones(n,k);
         cout << cant << endl;
         scanf("%i %i", &n, &k);
-    }
+    }*/
+
+    cout << cantSoluciones(8,6) << endl;
 
 	return 0;
 }
