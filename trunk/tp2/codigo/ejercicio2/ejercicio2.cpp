6bb19c0934cf0ae02db652737a27b2b4ca384f79
hadeskleizer <hadeskleizer@e3625ad9-2858-1949-73a1-c9317d294030>
2010-04-28 18:11:54 +0000

    Ahora si, estamos haciendo ford fulkerson y apra buscar el camino de aumento bellman ford. En algunos casos falla, como en el test2

--- trunk/tp2/codigo/ejercicio2/ejercicio2.cpp
+++ trunk/tp2/codigo/ejercicio2/ejercicio2.cpp
@@ -6,99 +6,164 @@ using namespace std;
 
 typedef unsigned int uint;
 
-inline int max( int a, int b )
+class Int
 {
-    if( a > b )
-        return a;
-    return b;
-}
+public:
+    Int(){ mInfinite = true; }
+    Int( int valor ){ mInfinite = false; mEntero = valor; }
+    void operator=( int valor ) { mInfinite = false; mEntero = valor; }
+    void operator=( Int int2 ) { mInfinite = int2.mInfinite; mEntero = int2.mEntero; }
+    bool operator>( Int int2 ) { if( !mInfinite && !int2.mInfinite ){ return mEntero > int2.mEntero; } return mInfinite && !int2.mInfinite; }
+    Int operator+( int valor ) { if( !mInfinite ) { return mEntero + valor; } return Int(); }
+    bool infinito() { return mInfinite; }
+    int valor() { return mEntero; }
+private:
+    bool mInfinite;
+    int mEntero;
+};
 
-inline int min( int a, int b )
-{
-    if( a > b )
-        return b;
-    return a;
-}
 
-class Grafo
+class Red
 {
 public:
 
-    typedef uint Sucesor;
-    typedef uint Peso;
-    typedef pair< Sucesor, Peso > Eje;
-    typedef vector< Eje > EjesConPesos;
-
-    Grafo(){};
-
-    Grafo( int cantidadNodos )
+    class Eje
     {
-        setCantidadNodos( cantidadNodos );
-    }
-    ~Grafo(){};
-
-    void agregarEje( uint nodo1, uint nodo2, uint peso )
-    {
-        uint ultimoNodo = listaAdyacencias.size() - 1;
-        if( nodo1 > ultimoNodo || nodo2 > ultimoNodo )
+    public:
+        Eje( uint fuente, uint destino )
         {
-            listaAdyacencias.resize( max( nodo1, nodo2 ) + 1 );
+            mFuente = fuente;
+            mDestino = destino;
         }
 
-        listaAdyacencias[ nodo1 ].push_back( pair< Sucesor, Peso >( nodo2, peso ) );
-        listaAdyacencias[ nodo2 ].push_back( pair< Sucesor, Peso >( nodo1, peso ) );
+        uint mFuente;
+        uint mDestino;
+    };
+
+    typedef vector< Eje > VEje;
+
+    Red( uint cantidadNodos )
+    {
+        mListaAdyacencias.resize( cantidadNodos );
+        mCostoEje = new uint* [ cantidadNodos ];
+        for( uint i = 0; i < cantidadNodos; i++ )
+        {
+            mCostoEje[ i ] = new uint [ cantidadNodos ];
+            for( uint j = 0; j < cantidadNodos; j++ )
+            {
+                mCostoEje[ i ][ j ] = 0;
+            }
+        }
+
+        mFlujoEje = new int* [ cantidadNodos ];
+        for( uint i = 0; i < cantidadNodos; i++ )
+        {
+            mFlujoEje[ i ] = new int [ cantidadNodos ];
+            for( uint j = 0; j < cantidadNodos; j++ )
+            {
+                mFlujoEje[ i ][ j ] = 0;
+            }
+        }
     }
 
-    const EjesConPesos& getAdyacentes( int nodo )
+    ~Red()
     {
-        return listaAdyacencias[ nodo ];
+        for( uint i = 0; i < mListaAdyacencias.size(); i++ )
+        {
+            delete mCostoEje[ i ];
+        }
+        delete mCostoEje;
+
+        for( uint i = 0; i < mListaAdyacencias.size(); i++ )
+        {
+            delete mFlujoEje[ i ];
+        }
+        delete mFlujoEje;
+    }
+
+    // agrega al grafo un eje dirigido de nodo1 -> nodo2
+    void agregarEje( uint nodo1, uint nodo2, uint costo, int flujo = 0 )
+    {
+        uint ultimoNodo = mListaAdyacencias.size() - 1;
+        if( nodo1 > ultimoNodo || nodo2 > ultimoNodo )
+        {
+            mListaAdyacencias.resize( max( nodo1, nodo2 ) + 1 );
+        }
+
+        mListaAdyacencias[ nodo1 ].push_back( Eje( nodo1, nodo2 ) );
+        mCostoEje[ nodo1 ][ nodo2 ] = costo;
+        mFlujoEje[ nodo1 ][ nodo2 ] = flujo;
+    }
+
+    void setFlujoEje( uint fuente, uint destino, int flujo )
+    {
+        mFlujoEje[ fuente ][ destino ] = flujo;
+    }
+
+    int getFlujoEje( uint fuente, uint destino )
+    {
+        return mFlujoEje[ fuente ][ destino ];
+    }
+
+    int getFlujoEje( const Eje& eje )
+    {
+        return mFlujoEje[ eje.mFuente ][ eje.mDestino ];
+    }
+
+    uint getCostoEje( uint fuente, uint destino )
+    {
+        return mCostoEje[ fuente ][ destino ];
+    }
+
+    uint getCostoEje( const Eje& eje )
+    {
+        return mCostoEje[ eje.mFuente ][ eje.mDestino ];
+    }
+
+    const VEje& getEjesSalida( int nodo )
+    {
+        return mListaAdyacencias[ nodo ];
     }
 
     uint getCantNodos()
     {
-        return listaAdyacencias.size();
+        return mListaAdyacencias.size();
     }
 
-    void setCantidadNodos( int cantidadNodos )
-    {
-        listaAdyacencias.resize( cantidadNodos );
-    }
+protected:
+    vector< VEje > mListaAdyacencias;
 
-    void clear()
-    {
-        listaAdyacencias.clear();
-    }
+    // matriz de costo de cada eje
+    uint** mCostoEje;
 
-private:
-    vector< EjesConPesos > listaAdyacencias;
+    // matriz de flujo de cada eje
+    int** mFlujoEje;
 };
 
 class TestCase
 {
     friend istream& operator>>( istream& is, TestCase& t )
     {
-        // primero limpio todas las variables miembro de t
-        t.mGrafo.clear();
-
         // obtengo el k correspondiente al enunciado
         int cantNodos;
         if( is >> cantNodos )
         {
+            t.mRed = new Red( cantNodos );
             int cantEjes;
             is >> cantEjes;
-            t.mGrafo.setCantidadNodos( cantNodos );
             for( int eje = 0; eje < cantEjes; eje++ )
             {
-                int nodo1, nodo2, peso;
+                uint nodo1, nodo2, costo;
                 is >> nodo1;
                 is >> nodo2;
-                is >> peso;
+                is >> costo;
 
-                // en la representación interna el mGrafo tiene nodo 0, por lo que debemos
+                // en la representación interna el mRed tiene nodo 0, por lo que debemos
                 // restarle 1 a los nodos pasados por parametro
                 nodo1--;
                 nodo2--;
-                t.mGrafo.agregarEje( nodo1, nodo2, peso );
+                t.mRed->agregarEje( nodo1, nodo2, costo );
+                t.mRed->agregarEje( nodo2, nodo1, costo );
             }
 
             is >> t.mCantidadData;
@@ -109,36 +174,23 @@ class TestCase
     }
 public:
     TestCase(){};
-    ~TestCase(){};
+    ~TestCase()
+    {
+        delete mRed;
+    }
 
-    int resolver()
+    long long int resolver()
     {
         // inicializamos la matriz de adyacencias auxiliar
-        int cantidadNodosGrafo = mGrafo.getCantNodos();
-        ejeUsado = new bool* [ cantidadNodosGrafo ];
-        for( int i = 0; i < cantidadNodosGrafo; i++ )
-        {
-            ejeUsado[ i ] = new bool [ cantidadNodosGrafo ];
-            for( int j = 0; j < cantidadNodosGrafo; j++ )
-            {
-                ejeUsado[ i ][ j ] = false;
-            }
-        }
+        int cantidadNodosGrafo = mRed->getCantNodos();
 
-        int tiempoTotal = 0;
         int cantDataEnviada = 0;
-        // Hacemos algo parecido a Ford Fulkerson
-        int cantTiempoUsado = buscarYEliminarCaminoMinimo( 0, cantidadNodosGrafo - 1 );
-        while( cantDataEnviada < mCantidadData && cantTiempoUsado >= 0 )
+
+        // Hacemos Ford Fulkerson
+        while( cantDataEnviada < mCantidadData && buscarCaminoYAumentar( 0, cantidadNodosGrafo - 1 ) )
         {
-            // sumamos el tiempo que se tarda en recorrer el camino encontrado
-            tiempoTotal += cantTiempoUsado * min( mCapacidadEnlaces, mCantidadData - cantDataEnviada );
-
-            // actualizamos la cantidad de datos restantes
+            // actualizamos la cantidad de datos enviados hasta el momento
             cantDataEnviada += mCapacidadEnlaces;
-
-            // buscamos un nuevo camino para el siguiente ciclo
-            cantTiempoUsado = buscarYEliminarCaminoMinimo( 0, cantidadNodosGrafo - 1 );
         }
 
         if( cantDataEnviada < mCantidadData )
@@ -146,107 +198,97 @@ public:
             return -1;
         }
 
+        long long int tiempoTotal = 0;
+        // Ahora calculamos el tiempo que nos costó enviar los datos de un punto a otro
+        for( int nodo = 0; nodo < cantidadNodosGrafo; nodo++ )
+        {
+            const Red::VEje& ejesSalida = mRed->getEjesSalida( nodo );
+            for( Red::VEje::const_iterator eje = ejesSalida.begin(); eje < ejesSalida.end(); eje++ )
+            {
+                int flujo = mRed->getFlujoEje( *eje );
+                if( flujo > 0 )
+                {
+                    tiempoTotal += mRed->getCostoEje( *eje ) * flujo;
+                }
+            }
+        }
+
         return tiempoTotal;
     }
 
 private:
 
-    int buscarYEliminarCaminoMinimo( int s, int t )
+    bool buscarCaminoYAumentar( int s, int t )
     {
-        vector< pair< int, int > > caminosMinimos;
+        vector< pair< Int, Int > > caminosMinimos;
         buscarCaminoMinimo( s, caminosMinimos );
 
         // si se encontro un camino hasta el final
-        if( caminosMinimos[ t ].first != -1 )
+        if( !caminosMinimos[ t ].first.infinito() )
         {
             int nodoActual = t;
 
             // marco los ejes que use para este camino
             while( nodoActual != s )
             {
-                int predecesor = caminosMinimos[ nodoActual ].first;
-                ejeUsado[ nodoActual ][ predecesor ] = true;
-                ejeUsado[ predecesor ][ nodoActual ] = true;
-                nodoActual = predecesor;
+                int fuente = caminosMinimos[ nodoActual ].first.valor();
+
+                int nuevoFlujo = mRed->getFlujoEje( fuente, nodoActual ) + min( mCapacidadEnlaces, mCantidadData );
+                mRed->setFlujoEje( fuente, nodoActual, nuevoFlujo );
+
+                nuevoFlujo = mRed->getFlujoEje( nodoActual, fuente ) - min( mCapacidadEnlaces, mCantidadData );
+                mRed->setFlujoEje( nodoActual, fuente, nuevoFlujo );
+                nodoActual = fuente;
             }
 
-            return caminosMinimos[ t ].second;
+            return !caminosMinimos[ t ].second.infinito();
         }
         else
         {
-            return -1;
+            return false;
         }
     }
 
-    void buscarCaminoMinimo( int s, vector< pair< int, int > >& resultado )
+    // Este algoritmo es una implementación de Bellman-Ford.
+    // Resultado es un vector de pares, que al finalizar la ejecución del algoritmo
+    // para i = 0, ..., n, siendo n la cantidad de nodos de la red,
+    // resultado[ i ].first representa el predecesor inmediato del camino de menor costo
+    // desde s hasta el nodo i, y resultado[ i ].second representa el costo acumulado para
+    // el camino de menor costo desde s hasta el nodo i.
+    void buscarCaminoMinimo( int s, vector< pair< Int, Int > >& resultado )
     {
-        // asignamos a cada nodo un valor de distancia 0 para el nodo inicial y -1 para todos los demas.
-        uint ultimoNodo = mGrafo.getCantNodos() - 1;
+        uint cantNodos = mRed->getCantNodos();
 
-        if( resultado.size() < ultimoNodo + 1 )
+        if( resultado.size() < cantNodos )
         {
-            resultado.resize( ultimoNodo + 1 );
+            resultado.resize( cantNodos );
         }
 
-        for( uint nodo = 0; nodo < ultimoNodo + 1; nodo++ )
-        {
-            resultado[ nodo ].first = -1;
-            resultado[ nodo ].second = -1;
-        }
-
-        // marcamos todos los nodos como no visitados
-        vector< bool > nodoVisitado( ultimoNodo + 1, false );
         resultado[ s ].second = 0;
 
-        // nodosAVisitar representa los nodos que falta visitar. Se inicializa con
-        // todos los nodos del mGrafo
-        vector< uint > nodosAVisitar( ultimoNodo + 1 );
-        for(uint i = 0; i < ultimoNodo + 1; i++ )
+        bool resultadoCambio = true;
+        for( uint i = 0; i < cantNodos && resultadoCambio; i++ )
         {
-            nodosAVisitar[i] = i;
-        }
-
-        // si todos los nodos ya fueron visitados, terminamos
-        while( !nodosAVisitar.empty() )
-        {
-            // buscamos el nodo no visitado que tenga menor distancia desde el inicio
-            vector< uint >::iterator itNodoActual = nodosAVisitar.begin();
-            int distNodoActual = resultado[ *itNodoActual ].second;
-
-            for( vector< uint >::iterator it = nodosAVisitar.begin(); it < nodosAVisitar.end(); it++ )
+            resultadoCambio = false;
+            for( uint nodo = 0; nodo < cantNodos; nodo++ )
             {
-                if( distNodoActual > resultado[ *it ].second && resultado[ *it ].second >= 0 )
+                const Red::VEje& ejesSalida = mRed->getEjesSalida( nodo );
+                for( Red::VEje::const_iterator eje = ejesSalida.begin(); eje < ejesSalida.end(); eje++ )
                 {
-                    distNodoActual = resultado[ *it ].second;
-                    itNodoActual = it;
-                }
-            }
-
-            // lo seteamos como nodo actual y lo marcamos como visitado
-            int nodoActual = *itNodoActual;
-            nodosAVisitar.erase( itNodoActual );
-            nodoVisitado[ nodoActual ] = true;
-
-            // consideramos todos sus sucesores y actualizamos la distancia y predecesor
-            const Grafo::EjesConPesos& sucesores = mGrafo.getAdyacentes( nodoActual );
-            for( Grafo::EjesConPesos::const_iterator itSucesores = sucesores.begin(); itSucesores < sucesores.end(); itSucesores++ )
-            {
-                int sucesor = itSucesores->first;
-                int peso = itSucesores->second;
-                if( !ejeUsado[ nodoActual ][ sucesor ] && \
-                    !nodoVisitado[ sucesor ]  && \
-                    ( resultado[ sucesor ].second > resultado[ nodoActual ].second + peso || resultado[ sucesor ].second == -1 ) )
-                {
-                    resultado[ sucesor ].second = resultado[ nodoActual ].second + peso;
-                    resultado[ sucesor ].first = nodoActual;
+                    Int distanciaFuente = resultado[ eje->mFuente ].second;
+                    Int distanciaDestino = resultado[ eje->mDestino ].second;
+                    uint costoEje = mRed->getCostoEje( *eje );
+                    int flujoEje = mRed->getFlujoEje( *eje );
+                    if( flujoEje < mCapacidadEnlaces && distanciaDestino > distanciaFuente + costoEje )
+                    {
+                        resultado[ eje->mDestino ].second = distanciaFuente + costoEje;
+                        resultado[ eje->mDestino ].first = eje->mFuente;
+                        resultadoCambio = true;
+                    }
                 }
             }
         }
     }
-
-    // es auxiliar, representa los ejes que van siendo usados y descartados para la siguiente iteracion
-    // durante el calculo del camino de aumento
-    bool** ejeUsado;
 
     // representa la cantidad de datos a enviar al otro punto de la red
     int mCantidadData;
@@ -254,16 +296,15 @@ private:
     // capacidad de los links de la red
     int mCapacidadEnlaces;
 
-    // mGrafo representativo de la red
-    Grafo mGrafo;
+    // mRed representativo de la red
+    Red* mRed;
 };
 
 int dataflow( istream& entrada, ostream& salida )
 {
-    TestCase testActual;
-    while( entrada >> testActual )
+    for(TestCase testActual; entrada >> testActual; )
     {
-        int resultado = testActual.resolver();
+        long long int resultado = testActual.resolver();
         if( resultado != -1 )
         {
             salida << resultado << endl;
@@ -278,14 +319,15 @@ int dataflow( istream& entrada, ostream& salida )
 }
 
 int main()
-{/*
-    ifstream in( "test", ios_base::in );
-    ofstream out( "testOut", ios_base::out );
-*/
-    int ret = dataflow( cin, cout );
-/*
+{
+    
+    ifstream in( "test2", ios_base::in );
+    ofstream out( "test2Out", ios_base::out );
+
+    int ret = dataflow( in, out );
+
     in.close();
     out.close();
-*/
+
     return ret;
 }
